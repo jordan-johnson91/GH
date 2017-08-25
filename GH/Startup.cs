0c65e5d3948b11bf1cd9b8b8b1360e58a60aea6f
Jordan Johnson <jordan@wecancodeit.org>
2017-08-25 14:15:52 -0400

    First Commit

---
+++ GH/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GH.Startup))]
+namespace GH
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

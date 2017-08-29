5572aab04839d278c60b96d579512a443b24561a
Jordan Johnson <jordan@wecancodeit.org>
2017-08-29 16:31:01 -0400

    Added Gig Form and added ViewModel for Gig Form

---
+++ GH/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GH.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        //This is going to be a dropd down list so we need a numeric value this is the Id from gig models
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
